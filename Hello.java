530f18b874b354acb42ad46976c4dc8ce5f15e8a
VaishnaviSapkale <vaishusapkale17@gmail.com>
2023-09-07 18:53:26 +0530

    uploading java files

---
+++ Hello.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Hello{
+      public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       char ch = 'A';
+      
+
+      switch(ch)
+      {
+            case 'A':
+            System.out.println("january");
+            break;
+            case 'B':
+            System.out.println("fab");
+            break;
+            case 'C':
+            System.out.println("march");
+           
+
+            default:
+            System.out.println("try another number");
+
+      }
+
+      
+        
+}
+} 
+
+
+
+
+
